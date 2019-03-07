b0a3cc777a2e4052ed72ce4742d035389c9665a2
oky saputra <saputraoky6@gmail.com>
2019-03-07 20:02:07 +0700

    add excersice

---
+++ basic/constant/example1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main()
+{
+    char a[20] = "oky saputra";
+    char z = '9';
+
+    if ( a[0] >= z) {
+        printf("Masuk Perulangan\n");
+    }
+
+    printf("a[0]  :%i \nz  :%i\n", a[0], z);
+    
+}
